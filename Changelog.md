d763353b562fbc9da220dec0b63b78a571907975
St. Frank <github@stephan-frank.de>
2025-01-24 23:21:01 +0100

    Compatibility with other extensions
    Reported by Anișor https://www.phpbb.com/community/viewtopic.php?p=16049530#p16049530
    Many thanks :)

--- Changelog.md
+++ Changelog.md
@@ -1,5 +1,11 @@
 # Changelog
 
+### 1.0.2 - 2025-01-24
+- Compatibility with other extensions
+
+Reported by Anișor https://www.phpbb.com/community/viewtopic.php?p=16049530#p16049530   
+Many thanks :) 
+
 ### 1.0.1 - 2025-01-23
 - Bug user profillink on avatar
 
--- event/listener.php
+++ event/listener.php
@@ -32,14 +32,18 @@ class listener implements EventSubscriberInterface
 	 */
 	public function __construct(\phpbb\auth\auth $auth)
 	{
-		$this->auth = $auth;
+		$this->auth	= $auth;
 	}
 
+	/**
+	 * Report on 24 Jan 2025 19:09 from Anișor, priority to 1
+	 * https://www.phpbb.com/community/viewtopic.php?p=16049530#p16049530
+	 */
 	public static function getSubscribedEvents()
 	{
 		return array(
-			'core.permissions'							=> 'core_add_permission',
-			'core.modify_username_string'				=> 'remove_profil_link',
+			'core.permissions' 		=> 'core_add_permission',
+			'core.modify_username_string'	=> ['remove_profile_link', 1],
 		);
 	}
 
@@ -53,22 +57,31 @@ class listener implements EventSubscriberInterface
 		$event['permissions'] = $permission;
 	}
 
-	public function remove_profil_link($vars)
+	/**
+	 * Report on 24 Jan 2025 19:09 from Anișor, Compatibility with Ext Verified Profiles
+	 * https://www.phpbb.com/community/viewtopic.php?p=16049602#p16049602
+	 */
+	public function remove_profile_link($event)
 	{
-		if (!$this->auth->acl_gets('a_hpl_view_profilelink', 'm_hpl_view_profilelink', 'u_hpl_view_profilelink'))
-		{
-			if ($vars['mode'] == 'full')
-			{
-				$username = strip_tags($vars['username_string']);
-				$colour = !empty($vars['username_colour']) ? ' style="color: ' . $vars['username_colour'] . ';"' : '';
+		$mode = $event['mode'];
+		$username_colour = $event['username_colour'];
+		$username_string = $event['username_string'];
+		$user_id = $event['user_id'];
 
-				$vars['username_string'] =  '<span' . $colour . ' class="username-coloured">' . $username . '</span>';
+		if (!$this->auth->acl_gets('a_hpl_view_profilelink', 'm_hpl_view_profilelink', 'u_hpl_view_profilelink', $user_id)) {
+			if ($mode == 'full')
+			{
+				$username = $event['username'];
+				$username_colour_code = ($username_colour) ? '' . $username_colour : '';
+				$username_string = $username_colour ? "<span style='color: {$username_colour_code};' class='username-coloured'>{$username}</span>" : "<span class='username'>{$username}</span>";
 			}
-
-			if ($vars['mode'] == 'profile')
+			else if ($mode == 'profile')
 			{
-				$vars['username_string'] ='';
+				$username_string = '';
 			}
 		}
+
+		//Send it back to the event
+		$event['username_string'] = $username_string;
 	}
 }
